0a0b170e1545732176250638e453a71f41e6fced
Samet <samet.yakut@metu.edu.tr>
2022-05-22 19:44:13 +0300

    Additions on second version

--- Magnetic Design/TransformerDesign_v2_EEcore.m
+++ Magnetic Design/TransformerDesign_v2_EEcore.m
@@ -12,7 +12,7 @@ Perm = 2500; % relative permittivity, same order with the excel sheet on github
 Le = 97e-3; % m
 Ae = 233e-6; % m^2, crossection of the core
 mu0 = 4*pi*10^-7; % permittivity of the air
-B = 0.1; % Tesla
+B = 0.15; % Tesla
 
 Iout = Pout/Vout;
 Iin = Pout/Vin;
@@ -27,9 +27,11 @@ Ilm_avg = Iin/D;
 
 % For CCM, DeltaIL/2<Ilm,avg
 Lm_min = Vin*D*fs^-1/(2*Ilm_avg);
-Lm = Lm_min*1.5;
+%Lm = Lm_min*1.5;
+Lm = 60e-6;
 DeltaIL = Vin*D*fs^-1/Lm; % Magnetizing Inductance Current Ripple
 Imax = Ilm_avg+DeltaIL;
+Imin = Ilm_avg-DeltaIL/2;
 
 Ipri_rms = Imax*sqrt(D);
 Isec_rms = Imax*sqrt(1-D);
@@ -39,6 +41,12 @@ CopperCross = pi*(0.5*0.425e-3)^2;% available in the laboratory, m^2
 Npri = ceil(Lm*Imax/(B*Ae));
 R = Npri^2/Lm;
 g = 0.5*mu0*Ae*(R-Le/(mu0*Perm*Ae)); % m
+g_mm = 1e3*g; %mm
+
+% Double check
+B_min = Npri*Imin/(R*Ae);
+B_max = Npri*Imax/(R*Ae);
+DeltaB = B_max-B_min;
 
 %%
 % Analytic Model of the Flyback Converter and Magnetic Design
@@ -55,7 +63,7 @@ Perm = 2500; % relative permittivity, same order with the excel sheet on github
 Le = 97e-3; % m
 Ae = 233e-6; % m^2, crossection of the core
 mu0 = 4*pi*10^-7; % permittivity of the air
-B = 0.1; % Tesla
+B = 0.15; % Tesla
 
 Iout = Pout/Vout;
 Iin = Pout/Vin;
@@ -70,18 +78,25 @@ Ilm_avg = Iin/D;
 
 % For CCM, DeltaIL/2<Ilm,avg
 Lm_min = Vin*D*fs^-1/(2*Ilm_avg);
-Lm = Lm_min*1.5;
+%Lm = Lm_min*1.5;
+Lm = 60e-6;
 DeltaIL = Vin*D*fs^-1/Lm; % Magnetizing Inductance Current Ripple
-Imax = Ilm_avg+DeltaIL;
+Imax = Ilm_avg+DeltaIL/2;
+Imin = Ilm_avg-DeltaIL/2;
 
 Ipri_rms = Imax*sqrt(D);
 Isec_rms = Imax*sqrt(1-D);
 CopperCross = pi*(0.5*0.425e-3)^2;% available in the laboratory, m^2
 
-
 Npri = ceil(Lm*Imax/(B*Ae));
 R = Npri^2/Lm;
 g = 0.5*mu0*Ae*(R-Le/(mu0*Perm*Ae)); % m
+g_mm = g*1e3; %mm
+
+% Double check
+B_min = Npri*Imin/(R*Ae);
+B_max = Npri*Imax/(R*Ae);
+DeltaB = B_max-B_min;
 
 %% COMMENTS
 % If there is no mistake, the third core (00K6527E060) will be suitable
